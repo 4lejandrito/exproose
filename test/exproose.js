2e4d665ec2fba9365a61be08c55aa7011274f71e
Alejandro Tardin <alejandro@tardin.com>
2015-02-22 19:53:10 +0000

    Added model method en exproose to create mongoose models

--- test/exproose.js
+++ test/exproose.js
@@ -7,6 +7,7 @@ var exproose = rekuire('src/exproose');
 var config = require('config');
 var db = rekuire('src/util/db');
 var express = require('express');
+var mongoose = rekuire('mongoose');
 
 describe("exproose", function() {
 
@@ -20,6 +21,17 @@ describe("exproose", function() {
 
     it(".config is the config from the config module", function() {
         expect(exproose).to.have.property('config').and.equal(config);
+    });
+
+    describe('model(name, schema)', function() {
+        it("returns a mongoose model class with the given schema", function() {
+            var Model = exproose.model('test', {
+                prop1: { type: String, required: true}
+            });
+            expect(Object.getPrototypeOf(Model)).to.eq(mongoose.Model);
+            expect(Model.modelName).to.eq('test');
+            expect(Model.schema.paths).to.have.property('prop1');
+        });
     });
 
     describe('()', function() {
